394bab89524a99b9857dd98a7b846888ed5100d0
Noskcaj <kcajdev@gmail.com>
2019-12-25 08:12:59 -0500

    Add inkwell to course item widget

--- lib/widgets/class_list_item_widget.dart
+++ lib/widgets/class_list_item_widget.dart
@@ -5,12 +5,14 @@ class ClassListItemWidget extends StatelessWidget {
       {@required this.course,
       @required this.teacher,
       @required this.letterGrade,
-      @required this.percent});
+      @required this.percent,
+      this.onTap});
 
   final String course;
   final String teacher;
   final String letterGrade;
   final num percent;
+  final void Function() onTap;
 
   Widget _buildColumn(
       String topText, String bottomText, CrossAxisAlignment alignment) {
@@ -30,15 +32,19 @@ class ClassListItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        color: Theme.of(context).primaryColor,
-        elevation: 3,
-        margin: EdgeInsets.all(15),
+      color: Theme.of(context).primaryColor,
+      elevation: 3,
+      margin: EdgeInsets.all(15),
+      child: InkWell(
+        onTap: onTap,
         child: Padding(
           padding: EdgeInsets.only(left: 10, right: 10, top: 25, bottom: 15),
           child: Row(children: <Widget>[
             _buildColumn(this.course, this.teacher, CrossAxisAlignment.start),
             _buildColumn('$percent%', this.letterGrade, CrossAxisAlignment.end)
           ]),
-        ));
+        ),
+      ),
+    );
   }
 }
