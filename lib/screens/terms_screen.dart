c7ed6a3f83cdf29e87691722f536a76ee9387308
Noskcaj <kcajdev@gmail.com>
2020-01-17 17:18:22 -0500

    Prevent swipe back gestures to the splash screen

--- lib/screens/terms_screen.dart
+++ lib/screens/terms_screen.dart
@@ -5,63 +5,68 @@ import 'package:shared_preferences/shared_preferences.dart';
 class TermsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xff2d3d54),
-      body: Center(
-        child: Column(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  const SizedBox(height: 40),
-                  const Text(
-                    "Welcome to SwiftGrade",
-                    style: TextStyle(
-                      color: Colors.white,
-                      letterSpacing: 1.0,
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'OpenSans',
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        backgroundColor: const Color(0xff2d3d54),
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 40),
+                    const Text(
+                      "Welcome to SwiftGrade",
+                      style: TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 1.0,
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'OpenSans',
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  // const Divider(
-                  //   thickness: 5,
-                  // ),
-                  const Expanded(
-                    flex: 1,
-                    child: SingleChildScrollView(
-                      padding: EdgeInsets.all(15.0),
-                      child: Text(
-                        TOS_STRING,
-                        style: TextStyle(
-                          color: Colors.white,
+                    const SizedBox(height: 10),
+                    // const Divider(
+                    //   thickness: 5,
+                    // ),
+                    const Expanded(
+                      flex: 1,
+                      child: SingleChildScrollView(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          TOS_STRING,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            RaisedButton(
-              child: const Text("I accept the terms and conditions",
-                  style: TextStyle(
-                    color: Colors.white,
-                    letterSpacing: 1.0,
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'OpenSans',
-                  )),
-              color: const Color(0xff2a84d2),
-              padding: const EdgeInsets.all(15.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
+              const SizedBox(height: 10),
+              RaisedButton(
+                child: const Text("I accept the terms and conditions",
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 1.0,
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'OpenSans',
+                    )),
+                color: const Color(0xff2a84d2),
+                padding: const EdgeInsets.all(15.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                onPressed: () => _acceptAndReturn(context),
               ),
-              onPressed: () => _acceptAndReturn(context),
-            ),
-            const SizedBox(height: 35),
-          ],
+              const SizedBox(height: 35),
+            ],
+          ),
         ),
       ),
     );
