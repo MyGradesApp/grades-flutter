ca0a65f115d70ac6ff8bd0c51def0187fcb8d9df
Seth <sethgoldin27@gmail.com>
2020-02-09 13:33:46 -0500

    updated app icon

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -4,9 +4,11 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:grades/models/current_session.dart';
 import 'package:grades/models/grade_persistence.dart';
+import 'package:grades/utilities/grades.dart';
 import 'package:grades/utilities/sentry.dart';
 import 'package:grades/utilities/stacked_future_builder.dart';
 import 'package:grades/widgets/class_list_item_widget.dart';
+import 'package:grades/widgets/colored_grade_dot.dart';
 import 'package:grades/widgets/loader_widget.dart';
 import 'package:grades/widgets/refreshable_error_message.dart';
 import 'package:pedantic/pedantic.dart';
@@ -23,87 +25,137 @@ class FeedScreen extends StatefulWidget {
 class _FeedScreenState extends State<FeedScreen> {
   Map<String, String> _newgrades = {};
 
-  // Future<List<Course>> _init() {
-  //   if (_newgrades == null) {
-  //     _newgrades = _setup();
-  //   }
-  //   return _newgrades;
-  // }
-
-  // Future<List<Course>> _setup() async {
-  //   var courses = await Provider.of<CurrentSession>(context, listen: false)
-  //       .sisLoader
-  //       .getCourses();
-
-  //   unawaited(
-  //     Future.wait(courses.map((course) async {
-  //       var grades = await course.getGrades();
-  //       setState(() {
-  //         _currentGrades[course.courseName] =
-  //             jsonEncode(grades, toEncodable: (v) => v.toString());
-  //       });
-  //     })),
-  //   );
-  //   return courses;
-  // }
-
-  // Future<List<Course>> _callback() async {
-  //   return Provider.of<CurrentSession>(context, listen: false)
-  //       .sisLoader
-  //       .getCourses(force: true);
-  // }
-
   @override
   Widget build(BuildContext context) {
     final gradePersistence = Provider.of<GradePersistence>(context);
-
-    return WillPopScope(
-      onWillPop: () async {
-        return false;
-      },
-      child: Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        appBar: AppBar(
-            elevation: 0.0,
-            centerTitle: true,
-            title: const Text('Recent'),
-            leading: IconButton(
-              tooltip: "Profile",
-              icon: Icon(
-                Icons.person,
-              ),
-              onPressed: () => Navigator.pushNamed(context, '/academic_info'),
-            )),
-        body: StackedFutureBuilder<List<Course>>(
-          // future: _init(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return RefreshIndicator(
-                // onRefresh: _callback,
-                child: ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    var course = snapshot.data[index];
-                    return ClassListItemWidget(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/course_grades',
-                            arguments: course);
-                      },
-                      course: course.courseName,
-                      letterGrade: course.gradeLetter,
-                      teacher: course.teacherName,
-                      percent: course.gradePercent,
-                    );
-                  },
-                ),
-              );
-            }
-            return Center(
-              child: LoaderWidget(),
-            );
-          },
-        ),
-      ),
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+          elevation: 0.0,
+          centerTitle: true,
+          title: const Text('Recent'),
+          leading: IconButton(
+            tooltip: "Profile",
+            icon: Icon(
+              Icons.person,
+            ),
+            onPressed: () => Navigator.pushNamed(context, '/academic_info'),
+          )),
+      body: StackedFutureBuilder<List<Course>>(builder: (context, snapshot) {
+        return SizedBox.expand();
+      }),
     );
+
+    //   return WillPopScope(
+    //     onWillPop: () async {
+    //       return false;
+    //     },
+    //     child: Scaffold(
+    //       backgroundColor: Theme.of(context).primaryColor,
+    //       appBar: AppBar(
+    //           elevation: 0.0,
+    //           centerTitle: true,
+    //           title: const Text('Recent'),
+    //           leading: IconButton(
+    //             tooltip: "Profile",
+    //             icon: Icon(
+    //               Icons.person,
+    //             ),
+    //             onPressed: () => Navigator.pushNamed(context, '/academic_info'),
+    //           )),
+    //       body: StackedFutureBuilder<List<Course>>(
+    //           builder: (context, snapshot) {
+    //         return SizedBox.expand(
+    //           child: SingleChildScrollView(
+    //             physics: const AlwaysScrollableScrollPhysics(),
+    //             child: Column(
+    //               children: [
+    //                 _buildCard("Unit 4 Test", Colors.white, Theme.of(context).accentColor),
+    //               ],
+    //             ),
+    //           ),
+    //         );
+    //       }),
+    //     ),
+    //   );
+    // }
+
+    // Widget _buildCard(BuildContext context, Map<String, dynamic> grade,
+    //     Color textColor, Color cardColor) {
+    //   var gradeString = grade["Grade"].toString();
+    //   var percentIndex = gradeString.indexOf('%');
+    //   String gradeLetter;
+    //   if (percentIndex != -1) {
+    //     var extractedGradePercent =
+    //         double.tryParse(gradeString.substring(0, percentIndex));
+
+    //     if (extractedGradePercent != null) {
+    //       gradeLetter = letterGradeForPercent(extractedGradePercent);
+    //     }
+    //   }
+    //   double gradeSize;
+    //   if (grade != null && gradeLetter != null) {
+    //     gradeSize = 50;
+    //   } else {
+    //     gradeSize = 100;
+    //   }
+
+    //   return  Card(
+    //     color: cardColor,
+    //     shape: RoundedRectangleBorder(
+    //       borderRadius: BorderRadius.circular(10.0),
+    //     ),
+    //     margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+    //     child: InkWell(
+    //       customBorder: RoundedRectangleBorder(
+    //         borderRadius: BorderRadius.circular(10.0),
+    //       ),
+    //       onTap: () {
+    //         Navigator.pushNamed(context, '/grades_detail', arguments: grade);
+    //       },
+    //       child: Padding(
+    //         padding: const EdgeInsets.all(17.0),
+    //         child: Row(
+    //           children: <Widget>[
+    //             Expanded(
+    //               child: Row(
+    //                 children: <Widget>[
+    //                   Text(
+    //                     grade["Assignment"] as String,
+    //                     style: TextStyle(color: textColor),
+    //                   ),
+    //                   const SizedBox(width: 6),
+    //                 ],
+    //               ),
+    //             ),
+    //             if (grade != null && gradeLetter != null)
+    //               ColoredGradeDot.grade(gradeLetter),
+    //             const SizedBox(width: 4),
+    //             if (gradeLetter != null)
+    //               Text(
+    //                 gradeLetter,
+    //                 style:
+    //                     TextStyle(color: textColor, fontWeight: FontWeight.bold),
+    //               ),
+    //             Container(
+    //               width: gradeSize,
+    //               alignment: Alignment.centerRight,
+    //               child: Text(
+    //                 gradeString,
+    //                 textAlign: TextAlign.end,
+    //                 style:
+    //                     TextStyle(color: textColor, fontWeight: FontWeight.bold),
+    //               ),
+    //             ),
+    //             const Icon(
+    //               Icons.chevron_right,
+    //               color: Colors.black26,
+    //               size: 18.0,
+    //             ),
+    //           ],
+    //         ),
+    //       ),
+    //     ),
+    //   );
   }
 }
--- lib/utilities/dots_indicator.dart
+++ lib/utilities/dots_indicator.dart
@@ -16,7 +16,7 @@ class DotsIndicator extends AnimatedWidget {
   final PageController controller;
   final int itemCount;
   final ValueChanged<int> onPageSelected;
-  final Color color = Colors.grey[300];
+  final Color color = const Color.fromRGBO(158, 158, 158, 0.5);
   static const double _kDotSize = 8.0;
   static const double _kMaxZoom = 2.0;
   static const double _kDotSpacing = 25.0;
