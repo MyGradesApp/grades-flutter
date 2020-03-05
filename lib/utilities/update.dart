bff03eafe89c5b9ac673070b7640bf1736f629b3
Noskcaj <kcajdev@gmail.com>
2020-03-05 16:50:24 -0500

    Add checkUpdateAvailable() function

---
+++ lib/utilities/update.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'sentry.dart' as sentry;
+
+Future<bool> checkUpdateAvailable() async {
+  var resp = await HttpClient()
+      .getUrl(Uri.parse(
+          "http://itunes.apple.com/lookup?bundleId=com.goldinguy.grades"))
+      .then((request) => request.close());
+
+  var body = await utf8.decoder.bind(resp).toList();
+  var dataStr = body.join();
+
+  var results = jsonDecode(dataStr)["results"];
+  if (results != null && results is List && results.isNotEmpty) {
+    var version = results[0]["version"];
+    if (version == null || version is! String) {
+      return false;
+    }
+
+    if (_compareVersions(sentry.version, version as String) ==
+        PartialOrdering.Greater) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+enum PartialOrdering {
+  Greater,
+  Equal,
+  Less,
+  Err,
+}
+
+PartialOrdering _compareVersions(String current, String other) {
+  var cSplit = current.split('\.');
+  var oSplit = other.split('\.');
+
+  if (cSplit.length != oSplit.length) {
+    return PartialOrdering.Err;
+  }
+  try {
+    for (int i = 0; i < cSplit.length; i += 1) {
+      var parsedCurrentPart = int.parse(cSplit[i]);
+      var parsedOtherPart = int.parse(oSplit[i]);
+      if (parsedCurrentPart < parsedOtherPart) {
+        return PartialOrdering.Greater;
+      } else if (parsedCurrentPart > parsedOtherPart) {
+        return PartialOrdering.Less;
+      }
+    }
+  } catch (e) {
+    return PartialOrdering.Err;
+  }
+  return PartialOrdering.Equal;
+}
