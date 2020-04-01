231e9982622d85b3f24e1bd618a08f2dd0f52fab
Noskcaj <kcajdev@gmail.com>
2020-03-31 22:04:51 -0400

    Initial upcoming screen implementation

---
+++ lib/widgets/upcoming_grade_item.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:grades/utilities/helpers/date.dart';
+import 'package:sis_loader/sis_loader.dart';
+
+class UpcomingGradeItem extends StatelessWidget {
+  final Grade grade;
+  final String courseName;
+  final Color textColor;
+  final Color cardColor;
+  final bool showIndicator;
+
+  UpcomingGradeItem(this.grade, this.textColor, this.cardColor,
+      this.showIndicator, this.courseName);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: cardColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: InkWell(
+        customBorder: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        onTap: () {
+          Navigator.pushNamed(context, '/grades_detail', arguments: grade);
+        },
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(17.0),
+                child: Text(
+                  grade.name,
+                  style: TextStyle(
+                    color: textColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  // TODO: Make it less aggressive about trimming
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        '${timeUntilHumanized(grade.dueDate)}',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          color: textColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  courseName,
+                ),
+              ],
+            ),
+            const Icon(
+              Icons.chevron_right,
+              color: Colors.black26,
+              size: 18.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
