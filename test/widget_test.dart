77dec063b697fd6f491f44ff944f4389d167fe4f
Noskcaj <kcajdev@gmail.com>
2020-03-31 22:31:57 -0400

    Make tests pass

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,25 +5,23 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:grades/main.dart';
+import 'package:grades/utilities/helpers/package_info.dart';
+import 'package:grades/utilities/sentry.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+Future<void> main() async {
+  // Setup
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final package_info = await getPackageInfo();
+  // Used for sentry error reporting and settings page version number
+  version = '${package_info.version}+${package_info.buildNumber}';
+
+  testWidgets('Launch test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
