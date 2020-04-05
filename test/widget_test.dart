c74095277223d849c8b5ff79cb11ab538bca9742
Noskcaj <kcajdev@gmail.com>
2020-04-05 15:29:44 -0400

    Make tests pass

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,25 +7,30 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:grades/blocs/offline/offline_bloc.dart';
+import 'package:grades/repos/sis_repository.dart';
 import 'package:grades/main.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grades/blocs/authentication/authentication_bloc.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(App(
-      sisRepository: null,
+  testWidgets('App smoke test', (WidgetTester tester) async {
+    var offlineBloc = OfflineBloc();
+    var sisRepository = SISRepository(offlineBloc);
+
+    await tester.pumpWidget(MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthenticationBloc(sisRepository: sisRepository)
+            ..add(AppStarted()),
+        ),
+        BlocProvider(
+          create: (context) => offlineBloc,
+        ),
+      ],
+      child: App(
+        sisRepository: sisRepository,
+      ),
     ));
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
