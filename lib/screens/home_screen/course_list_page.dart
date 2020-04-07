5d4981d9ee000776d1c5ab0013f36811c7a027c8
Noskcaj <kcajdev@gmail.com>
2020-04-06 20:09:11 -0400

    Implement refresh indicator for course list

--- lib/screens/home_screen/course_list_page.dart
+++ lib/screens/home_screen/course_list_page.dart
@@ -1,26 +1,39 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:grades/blocs/course_list/course_list_bloc.dart';
 import 'package:grades/blocs/network_action_bloc/network_action_bloc.dart';
 import 'package:sis_loader/sis_loader.dart';
 
-class CourseListPage extends StatelessWidget {
+class CourseListPage extends StatefulWidget {
+  @override
+  _CourseListPageState createState() => _CourseListPageState();
+}
+
+class _CourseListPageState extends State<CourseListPage> {
+  Completer<void> _refreshCompleter = Completer();
+
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CourseListBloc, NetworkActionState>(
-      builder: (context, state) {
-        if (state is NetworkLoading) {
-          return Center(child: CircularProgressIndicator());
-        }
-        if (state is NetworkLoaded<List<Course>>) {
-          return RefreshIndicator(
-            onRefresh: () {
-              // TODO: Future this up
-              BlocProvider.of<CourseListBloc>(context)
-                  .add(RefreshNetworkData());
-              return Future.value();
-            },
-            child: ListView.builder(
+    return RefreshIndicator(
+      onRefresh: () {
+        BlocProvider.of<CourseListBloc>(context).add(RefreshNetworkData());
+        return _refreshCompleter.future;
+      },
+      child: BlocConsumer<CourseListBloc, NetworkActionState>(
+        listener: (BuildContext context, NetworkActionState state) {
+          if (state is NetworkLoaded) {
+            _refreshCompleter?.complete();
+            _refreshCompleter = Completer();
+          }
+        },
+        builder: (context, state) {
+          if (state is NetworkLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (state is NetworkLoaded<List<Course>>) {
+            return ListView.builder(
               itemCount: state.data.length,
               itemBuilder: (context, i) {
                 var course = state.data[i];
@@ -40,24 +53,24 @@ class CourseListPage extends StatelessWidget {
                   ),
                 );
               },
-            ),
-          );
-        }
-        if (state is NetworkError) {
-          return RefreshIndicator(
-            onRefresh: () {
-              BlocProvider.of<CourseListBloc>(context)
-                  .add(RefreshNetworkData());
-              return Future.value();
-            },
-            child: SingleChildScrollView(
-              physics: AlwaysScrollableScrollPhysics(),
-              child: Center(child: Text('An error occurred')),
-            ),
-          );
-        }
-        return Container();
-      },
+            );
+          }
+          if (state is NetworkError) {
+            return RefreshIndicator(
+              onRefresh: () {
+                BlocProvider.of<CourseListBloc>(context)
+                    .add(RefreshNetworkData());
+                return Future.value();
+              },
+              child: SingleChildScrollView(
+                physics: AlwaysScrollableScrollPhysics(),
+                child: Center(child: Text('An error occurred')),
+              ),
+            );
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
