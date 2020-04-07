62a4ac2eac7f5a57962a844355263cf79ef8e631
Noskcaj <kcajdev@gmail.com>
2020-04-06 20:19:05 -0400

    Implement refresh indicator for grades screen

--- lib/screens/course_grades/course_grades_view.dart
+++ lib/screens/course_grades/course_grades_view.dart
@@ -1,42 +1,64 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:grades/blocs/course_grades/course_grades_bloc.dart';
 import 'package:grades/blocs/network_action_bloc/network_action_bloc.dart';
 import 'package:sis_loader/sis_loader.dart';
 
-class CourseGradesView extends StatelessWidget {
+class CourseGradesView extends StatefulWidget {
+  @override
+  _CourseGradesViewState createState() => _CourseGradesViewState();
+}
+
+class _CourseGradesViewState extends State<CourseGradesView> {
+  Completer<void> _refreshCompleter = Completer<void>();
+
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CourseGradesBloc, NetworkActionState>(
-      builder: (BuildContext context, state) {
-        if (state is NetworkLoading) {
-          return CircularProgressIndicator();
-        }
-        if (state is NetworkError) {
-          return Text('An error occured');
-        }
-        if (state is NetworkLoaded<List<Grade>>) {
-          return ListView.builder(
-            shrinkWrap: true,
-            itemCount: state.data.length,
-            itemBuilder: (context, i) {
-              var grade = state.data[i];
-              return RaisedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/grade_info', arguments: grade);
-                },
-                child: Row(
-                  children: <Widget>[
-                    Expanded(child: Text(grade.name)),
-                    Text(grade.grade)
-                  ],
-                ),
-              );
-            },
-          );
-        }
-        return Container();
+    return RefreshIndicator(
+      onRefresh: () {
+        BlocProvider.of<CourseGradesBloc>(context).add(RefreshNetworkData());
+        return _refreshCompleter.future;
       },
+      child: BlocConsumer<CourseGradesBloc, NetworkActionState>(
+        listener: (BuildContext context, state) {
+          if (state is NetworkLoaded || state is NetworkError) {
+            _refreshCompleter?.complete();
+            _refreshCompleter = Completer();
+          }
+        },
+        builder: (BuildContext context, state) {
+          if (state is NetworkLoading) {
+            return CircularProgressIndicator();
+          }
+          if (state is NetworkError) {
+            return Text('An error occured');
+          }
+          if (state is NetworkLoaded<List<Grade>>) {
+            return ListView.builder(
+              shrinkWrap: true,
+              itemCount: state.data.length,
+              itemBuilder: (context, i) {
+                var grade = state.data[i];
+                return RaisedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/grade_info',
+                        arguments: grade);
+                  },
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: Text(grade.name)),
+                      Text(grade.grade)
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
