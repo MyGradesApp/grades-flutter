5e9be2de9f88886cc28e5e7f0e157ad18c9a6fc0
Noskcaj <kcajdev@gmail.com>
2020-04-20 21:17:29 -0400

    Style splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -4,16 +4,36 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Splash Screen'),
-      ),
       body: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 40.0,
+          vertical: 120.0,
+        ),
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Text('Swift Grade'),
-              CircularProgressIndicator(),
+              const Text(
+                'SwiftGrade',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'OpenSans',
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 25.0),
+              const Text(
+                'Your grades at a glance',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'OpenSans',
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 50),
+              const CircularProgressIndicator(),
             ],
           ),
         ),
