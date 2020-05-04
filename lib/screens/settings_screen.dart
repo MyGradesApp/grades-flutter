f117bca7b5be3b9ca380c4c7483f5cff78b9b778
Noskcaj <kcajdev@gmail.com>
2020-05-03 20:05:59 -0400

    Style settings page

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:grade_core/grade_core.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:grades/utilities/update.dart';
+
+const TextStyle HEADER_TEXT_STYLE = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+);
 
 class SettingsScreen extends StatelessWidget {
   @override
@@ -11,50 +18,162 @@ class SettingsScreen extends StatelessWidget {
         elevation: 0.0,
         title: Text('Settings'),
       ),
-      body: Column(
-        children: <Widget>[
-          RaisedButton(
-            onPressed: () {
-              BlocProvider.of<AuthenticationBloc>(context).add(LoggedOut());
-              Navigator.pop(context);
-            },
-            child: Text('Log out'),
+      body: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (BuildContext context, settings) {
+          return ListView(
+            children: [
+              FutureBuilder<bool>(
+                future: checkUpdateAvailable(),
+                builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                  if (snapshot.hasData && Platform.isIOS) {
+                    return _buildCard(
+                      accentColor: const Color.fromARGB(255, 211, 117, 116),
+                      child: Row(children: [
+                        const Expanded(
+                          child: Text(
+                            'Update Available',
+                            style: HEADER_TEXT_STYLE,
+                          ),
+                        ),
+                        const Text(
+                          'Click to update now',
+                          style: TextStyle(
+                            fontSize: 17,
+                          ),
+                        ),
+                      ]),
+                      onPressed: () {
+                        launchAppstorePage();
+                      },
+                    );
+                  }
+                  return Container();
+                },
+              ),
+              BlocBuilder<ThemeBloc, ThemeMode>(
+                builder: (BuildContext context, theme) => _buildCard(
+                  child: Row(children: [
+                    const Expanded(
+                      child: Text(
+                        'App Theme',
+                        style: HEADER_TEXT_STYLE,
+                      ),
+                    ),
+                    Text(
+                      theme.toHumanString(),
+                      style: TextStyle(
+                        fontSize: 17,
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Icon(
+                      () {
+                        switch (theme) {
+                          case ThemeMode.system:
+                            return Icons.computer;
+                            break;
+                          case ThemeMode.light:
+                            return Icons.wb_sunny;
+                            break;
+                          case ThemeMode.dark:
+                            return Icons.brightness_2;
+                            break;
+                          default:
+                            throw Exception();
+                        }
+                      }(),
+                    ),
+                  ]),
+                  onPressed: () {
+                    BlocProvider.of<ThemeBloc>(context).add(AdvanceTheme());
+                  },
+                ),
+              ),
+              _buildCard(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        'Default Sort Style',
+                        style: HEADER_TEXT_STYLE,
+                      ),
+                    ),
+                    Text(
+                      settings.groupingMode == GroupingMode.Date
+                          ? 'Recency'
+                          : 'Category',
+                      style: TextStyle(
+                        fontSize: 17,
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Icon(
+                      settings.groupingMode == GroupingMode.Date
+                          ? Icons.today
+                          : Icons.format_list_bulleted,
+                    ),
+                  ],
+                ),
+                onPressed: () => BlocProvider.of<SettingsBloc>(context)
+                    .add(ToggleSettingsGroupingMode()),
+              ),
+              _buildCard(
+                child: Text(
+                  'Terms of Service',
+                  style: HEADER_TEXT_STYLE,
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/terms_settings');
+                },
+              ),
+              _buildCard(
+                child: Text(
+                  'Sign Out',
+                  style: HEADER_TEXT_STYLE,
+                ),
+                onPressed: () {
+                  BlocProvider.of<AuthenticationBloc>(context).add(LoggedOut());
+                  Navigator.pop(context);
+                },
+              ),
+              Center(
+                child: Text(
+                  GRADES_VERSION,
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildCard({
+    @required Widget child,
+    @required void Function() onPressed,
+    Color accentColor,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        color: accentColor,
+        child: FlatButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
           ),
-          RaisedButton(
-            onPressed: () {
-              BlocProvider.of<ThemeBloc>(context).add(AdvanceTheme());
-            },
-            child: BlocBuilder<ThemeBloc, ThemeMode>(
-              builder: (context, themeMode) {
-                return Text('Theme: ${themeMode.toHumanString()}');
-              },
+          onPressed: onPressed,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8.0, top: 26, bottom: 26),
+            child: SizedBox(
+              width: double.infinity,
+              child: child,
             ),
           ),
-          RaisedButton(
-            onPressed: () async {
-              var prefs = await SharedPreferences.getInstance();
-              await prefs.remove(DataPersistence.GRADES_KEY);
-              await prefs.remove(DataPersistence.COURSES_KEY);
-              await prefs.remove(DataPersistence.ACADEMIC_INFO_KEY);
-            },
-            child: BlocBuilder<ThemeBloc, ThemeMode>(
-              builder: (context, themeMode) {
-                return Text('Clear persistence data');
-              },
-            ),
-          ),
-          RaisedButton(
-            onPressed: () {
-              BlocProvider.of<SettingsBloc>(context)
-                  .add(ToggleSettingsGroupingMode());
-            },
-            child: BlocBuilder<SettingsBloc, SettingsState>(
-              builder: (context, settings) {
-                return Text('Grouping: ${settings.groupingMode.asString()}');
-              },
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
