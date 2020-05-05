ffb44779a0fc69880b89e8e6f4ba3cfa57ea1a34
Noskcaj <kcajdev@gmail.com>
2020-05-05 15:05:47 -0400

    Add page indicator

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:grade_core/grade_core.dart';
 import 'package:grades/screens/home_screen/course_list_page.dart';
 import 'package:grades/screens/home_screen/recent_page.dart';
 import 'package:grades/screens/home_screen/upcoming_page.dart';
+import 'package:grades/widgets/page_indicator.dart';
 import 'package:tuple/tuple.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -60,19 +61,38 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: MultiBlocProvider(
-        providers: providers ??=
-            _buildProviders(RepositoryProvider.of<SISRepository>(context)),
-        child: PageView.builder(
-          controller: controller,
-          itemCount: pages.length,
-          onPageChanged: (i) {
-            setState(() {
-              title = pages[i].item1;
-            });
-          },
-          itemBuilder: (context, position) => pages[position].item2,
-        ),
+      body: Stack(
+        children: [
+          MultiBlocProvider(
+            providers: providers ??=
+                _buildProviders(RepositoryProvider.of<SISRepository>(context)),
+            child: PageView.builder(
+              controller: controller,
+              itemCount: pages.length,
+              onPageChanged: (i) {
+                setState(() {
+                  title = pages[i].item1;
+                });
+              },
+              itemBuilder: (context, position) => pages[position].item2,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            alignment: Alignment.bottomCenter,
+            child: PageIndicator(
+              controller: controller,
+              itemCount: pages.length,
+              onPageSelected: (int page) {
+                controller.animateToPage(
+                  page,
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.ease,
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/widgets/page_indicator.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+/// An indicator showing the currently selected page of a PageController
+class PageIndicator extends AnimatedWidget {
+  PageIndicator({
+    this.controller,
+    this.itemCount,
+    this.onPageSelected,
+  }) : super(listenable: controller);
+
+  final PageController controller;
+  final int itemCount;
+  final ValueChanged<int> onPageSelected;
+  final Color color = const Color.fromRGBO(185, 185, 185, .6);
+  static const double _kDotSize = 8.0;
+  static const double _kMaxZoom = 2.0;
+  static const double _kDotSpacing = 25.0;
+
+  Widget _buildDot(int index) {
+    var selectedness = Curves.easeOut.transform(
+      max(
+        0.0,
+        1.0 - ((controller.page ?? controller.initialPage) - index).abs(),
+      ),
+    );
+    var zoom = 1.0 + (_kMaxZoom - 1.0) * selectedness;
+    return Container(
+      width: _kDotSpacing,
+      child: Material(
+        color: color,
+        type: MaterialType.circle,
+        child: Container(
+          width: _kDotSize * zoom,
+          height: _kDotSize * zoom,
+          child: InkWell(
+            onTap: () => onPageSelected(index),
+            customBorder: const CircleBorder(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List<Widget>.generate(itemCount, _buildDot),
+    );
+  }
+}
