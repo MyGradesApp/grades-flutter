5d5d760568918aeecb5cdabaf74b7ae73c440949
Seth <sethgoldin27@gmail.com>
2020-09-03 01:34:53 -0400

    significantly clean up grade calculator, now calculates empty categories properly

--- lib/screens/course_grades/grade_calc/calculate_class_percent.dart
+++ lib/screens/course_grades/grade_calc/calculate_class_percent.dart
@@ -4,51 +4,33 @@ import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:grades/screens/course_grades/course_grades_view.dart';
 import 'package:grades/screens/course_grades/grade_calc/dummy_grades.dart';
-import 'package:quiver/pattern.dart';
 import 'package:sis_loader/sis_loader.dart';
 
 double calculateClassPercent(Map<ToHeader, List<Grade>> groupedGrades,
     BuiltMap<String, String> weights) {
   var groupKeys = groupedGrades.keys.toList()..sort();
   var classPercent = 0.0, gradesCount = 0.0, weightedDenominatorList = <int>[];
-  var weightedList = <Map<String, Grade>>[];
-  var containsOnlyConduct = false;
+  var weightedGradeList = <Map<String, Map<String, Grade>>>[];
 
-  // for every grade:
   for (var group in groupKeys) {
     for (var gradeItem in groupedGrades[group]) {
       if (weights != null) {
-        // for each weight, add it to a "weighted" list
         for (var weight in weights.entries) {
           if (weight.value != '0%') {
-            // print('we' + weight.value);
-
             if (weight.key.contains(gradeItem.category)) {
-              // if a category is empty, use the following to determine new percentages
-
-              var x = 0;
-              weightedList.forEach((grade) {
-                if (grade.containsKey(weight.key)) {
-                  x++;
-                }
-              });
-
-              if (x == 0) {
+              if (weightedGradeList
+                  .every((item) => !(item.containsKey(weight.key)))) {
                 weightedDenominatorList.add(int.tryParse(
                     weight.value.substring(0, weight.value.indexOf('%'))));
-                print(weightedDenominatorList);
               }
 
-              weightedList.add({weight.key: gradeItem});
-            }
-          } else {
-            if (weightedList.isEmpty) {
-              containsOnlyConduct = true;
+              weightedGradeList.add({
+                weight.key: {weight.value: gradeItem}
+              });
             }
           }
         }
       } else {
-        // if a class has no grades, add it to the class as normal
         var index = gradeItem.grade.indexOf('%');
         if (index != -1) {
           var gradePercent =
@@ -59,68 +41,46 @@ double calculateClassPercent(Map<ToHeader, List<Grade>> groupedGrades,
       }
     }
   }
+  classPercent = classPercent / (gradesCount != 0 ? gradesCount : 1);
 
-  if (weights != null) {
-    for (var weight in weights.entries) {
-      if (weight.value != '0%' && weightedList.isNotEmpty) {
-        var groupTotal = 0.0, weightedGroupCount = 0;
-        for (var weightedItem in weightedList) {
-          // print(weight.value);
-          if (weightedItem.containsKey(weight.key)) {
-            var index = weightedItem.values.last.grade.indexOf('%');
-            if (index != -1) {
-              var gradePercent = double.tryParse(
-                  weightedItem.values.last.grade.substring(0, index));
-              // print('grade');
-              // print(gradePercent);
-              groupTotal += gradePercent;
-              weightedGroupCount++;
-            }
+  // print(weightedGradeList);
+  // print(weightedDenominatorList);
+
+  if (weights != null && weightedDenominatorList.isNotEmpty) {
+    weightedDenominatorList.forEach((weight) {
+      var groupTotal = 0.0;
+      gradesCount = 0;
+      weightedGradeList.forEach((weightedItem) {
+        if (weightedItem.values.first.containsKey(weight.toString() + '%')) {
+          var index = weightedItem.values.last.values.last.grade.indexOf('%');
+          if (index != -1) {
+            var gradePercent = double.tryParse(
+                weightedItem.values.last.values.last.grade.substring(0, index));
+            groupTotal += gradePercent;
+            gradesCount++;
           }
         }
-        // TODO: determine accurate way of finding percentage when categories have no grades (for now, just multiply category weight by 100)
-        if (weightedGroupCount > 0) {
-          // print('group bef div' + groupTotal.toString());
-          groupTotal = groupTotal / weightedGroupCount;
-        }
-        if (weights.entries.length != weightedDenominatorList.length) {
-          var weightedDenominator = 0.0;
-          for (var den in weightedDenominatorList) {
-            weightedDenominator += den;
-          }
-          print('group');
-          print(groupTotal);
-          groupTotal = groupTotal *
-              (double.tryParse(
-                      weight.value.substring(0, weight.value.indexOf('%'))) /
-                  weightedDenominator);
-          print((weight.value.substring(0, weight.value.indexOf('%')))
-                  .toString() +
-              '/' +
-              weightedDenominator.toString());
-        } else {
-          groupTotal = groupTotal *
-              (double.tryParse(
-                      weight.value.substring(0, weight.value.indexOf('%'))) /
-                  100.0);
-        }
-
-        classPercent += groupTotal;
+      });
+      groupTotal = groupTotal / (gradesCount != 0 ? gradesCount : 1);
+      if (weights.entries.length != weightedDenominatorList.length) {
+        groupTotal = groupTotal *
+            (weight / weightedDenominatorList.fold(0, (num p, c) => p + c));
+        // print((weight.toString() +
+        //     '/' +
+        //     (weightedDenominatorList.fold(0, (num p, c) => p + c).toString())));
+      } else {
+        groupTotal = groupTotal * (weight / 100);
       }
-    }
-
-    if (classPercent == 0.0 &&
-        weights.containsValue('0%') &&
-        containsOnlyConduct) {
-      classPercent = -1;
-    }
+      classPercent += groupTotal;
+    });
   }
 
-  if (gradesCount > 0) {
-    classPercent = classPercent / gradesCount;
+  if (classPercent == 0.0 &&
+      weightedGradeList.every((item) => item.containsKey('Conduct'))) {
+    classPercent = -1;
   }
 
-  print(classPercent);
+  // print(classPercent);
   return classPercent;
 }
 
