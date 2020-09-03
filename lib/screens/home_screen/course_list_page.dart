0d40396238a362af5042e9969c959212e7fb1bae
Seth <sethgoldin27@gmail.com>
2020-09-03 12:20:34 -0400

    add update card to course list page

--- lib/screens/home_screen/course_list_page.dart
+++ lib/screens/home_screen/course_list_page.dart
@@ -1,13 +1,17 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:grade_core/grade_core.dart';
+import 'package:grades/utilities/update.dart';
 import 'package:grades/widgets/class_list_item.dart';
 import 'package:grades/widgets/loading_indicator.dart';
 import 'package:grades/widgets/refreshable/fullscreen_error_message.dart';
 import 'package:grades/widgets/refreshable/fullscreen_simple_icon_message.dart';
+
+import '../settings_screen.dart';
 
 class CourseListPage extends StatefulWidget {
   @override
@@ -52,6 +56,24 @@ class _CourseListPageState extends State<CourseListPage> {
               itemCount: state.data.length,
               itemBuilder: (context, i) {
                 var course = state.data[i];
+                if (i == 0) {
+                  return Column(children: [
+                    getUpdateCard(),
+                    ClassListItem(
+                      course: course.courseName,
+                      letterGrade: course.gradeLetter,
+                      percent: course.gradePercent,
+                      teacher: course.teacherName,
+                      onTap: () {
+                        Navigator.pushNamed(
+                          context,
+                          '/course_grades',
+                          arguments: course,
+                        );
+                      },
+                    )
+                  ]);
+                }
                 return ClassListItem(
                   course: course.courseName,
                   letterGrade: course.gradeLetter,
@@ -85,6 +107,56 @@ class _CourseListPageState extends State<CourseListPage> {
           return Container();
         },
       ),
+    );
+  }
+
+  Widget getUpdateCard() {
+    return FutureBuilder<bool>(
+      future: checkUpdateAvailable(),
+      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+        if (snapshot.hasData && snapshot.data && Platform.isIOS) {
+          return Padding(
+            padding: const EdgeInsets.all(4),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              color: Color.fromARGB(255, 211, 117, 116),
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                onPressed: () {
+                  launchAppstorePage();
+                },
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 8.0, top: 26, bottom: 26),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Row(children: [
+                      const Expanded(
+                        child: Text(
+                          'Update Available',
+                          style: HEADER_TEXT_STYLE,
+                        ),
+                      ),
+                      const Text(
+                        'Click to update now',
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ]),
+                  ),
+                ),
+              ),
+            ),
+          );
+        }
+        return Container();
+      },
     );
   }
 }
