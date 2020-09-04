b118de52327a0a579895722acaef3a03b95e4dbb
Seth <sethgoldin27@gmail.com>
2020-09-04 12:47:23 -0400

    comment out open sis button

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -125,15 +125,15 @@ class SettingsScreen extends StatelessWidget {
                 onPressed: () => BlocProvider.of<SettingsBloc>(context)
                     .add(ToggleSettingsGroupingMode()),
               ),
-              _buildCard(
-                child: Text(
-                  'Open SIS',
-                  style: HEADER_TEXT_STYLE,
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/sis_webview');
-                },
-              ),
+              // _buildCard(
+              //   child: Text(
+              //     'Open SIS',
+              //     style: HEADER_TEXT_STYLE,
+              //   ),
+              //   onPressed: () {
+              //     Navigator.pushNamed(context, '/sis_webview');
+              //   },
+              // ),
               _buildCard(
                 child: Text(
                   'Terms of Service',
